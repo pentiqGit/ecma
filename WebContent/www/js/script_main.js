6a8454733150744c940a8e724e30e40e3f304f33
ikue <ikue@192.168.0.132>
2017-05-07 13:37:49 +0900

    新規作成

---
+++ WebContent/www/js/script_main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var $ = require('jQuery');
+
+$(function() {
+	var inputElement = $('#input');
+	inputElement.on('focusout', function(){
+		var input = inputElement.val();
+		console.log("input=", input);
+
+		$("#output").text(input);
+	})
+})
